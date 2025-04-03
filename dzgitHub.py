37e5d53c66774f34884f1d199b9139cf09acbbae
Oleksandr Torbik <torbikolexandr@gmail.com>
2025-04-03 12:36:26 +0300

    Find_max_len

--- dzgitHub.py
+++ dzgitHub.py
@@ -1,6 +1,12 @@
 def open_file():
     with open('input.txt', 'w') as file:
-        print('Python\nRuby\nC++\nC\nJava\nGO', file=file)
+        print('Python\nRuby\nC++len08\nC\nJava\nGO', file=file)
 
+def overkill():
+    with open('input.txt', 'r') as file:
+        memory = ""
+        for line in file:
+            if len(line) > len(memory):
+                memory = line
+    return memory
 
-open_file()
